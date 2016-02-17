ee476769e3add1943625f6fa3f32910f5155c748
Dmitry Baev <charlie@yandex-team.ru>
2016-02-17 23:29:10 +0300

    cleanup

--- allure-report-api/src/main/java/org/allurefw/report/ReportDataProvider.java
+++ allure-report-api/src/main/java/org/allurefw/report/ReportDataProvider.java
@@ -5,24 +5,10 @@ package org.allurefw.report;
  *         Date: 01.02.16
  */
 
-//TODO typed? Maybe we should use generics?
 public interface ReportDataProvider {
 
     Object provide();
 
     String getFileName();
 
-    static ReportDataProvider provider(String name, Object object) {
-        return new ReportDataProvider() {
-            @Override
-            public Object provide() {
-                return object;
-            }
-
-            @Override
-            public String getFileName() {
-                return name;
-            }
-        };
-    }
 }
