48d67e3d37fe9ef29d2b02954481893da4123d4f
Dmitry Baev <baev.dm@gmail.com>
2016-09-20 16:13:35 +0300

    fix tests

--- generator/src/test/java/org/allurefw/report/MainTest.java
+++ generator/src/test/java/org/allurefw/report/MainTest.java
@@ -1,12 +1,18 @@
 package org.allurefw.report;
 
+import com.google.common.reflect.ClassPath;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -21,18 +27,42 @@ public class MainTest {
     public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
-    public void shouldGenerateTheReport() throws Exception {
+    public void shouldCreateReportInfo() throws Exception {
         Path plugins = folder.newFolder().toPath();
         Path work = folder.newFolder().toPath();
         Main main = new Main(plugins, work, Collections.emptySet());
 
-        Path results = Paths.get("/Users/charlie/projects/allure-report/generator/src/test/resources/allure1data");
-        ReportInfo report = main.createReport(results);
+        ReportInfo report = main.createReport(getDataFolder("allure1data/"));
 
         assertThat(report, notNullValue());
         assertThat(report.getResults(), hasSize(20));
+    }
 
-        Path output = Paths.get("/Users/charlie/projects/allure-report/generator/target/new-report");
-        main.generate(output, results);
+    @Test
+    public void shouldGenerateTheReport() throws Exception {
+        Path plugins = folder.newFolder().toPath();
+        Path work = folder.newFolder().toPath();
+        Main main = new Main(plugins, work, Collections.emptySet());
+        main.generate(folder.newFolder().toPath(), getDataFolder("allure1data/"));
+    }
+
+    private Path getDataFolder(String prefix) throws IOException {
+        ClassPath classPath = ClassPath.from(getClass().getClassLoader());
+        Map<String, URL> files = classPath.getResources().stream()
+                .filter(info -> info.getResourceName().startsWith(prefix))
+                .collect(Collectors.toMap(
+                        info -> info.getResourceName().substring(prefix.length()),
+                        ClassPath.ResourceInfo::url)
+                );
+        Path dir = folder.newFolder().toPath();
+        files.forEach((name, url) -> {
+            Path file = dir.resolve(name);
+            try (InputStream is = url.openStream()) {
+                Files.copy(is, file);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        return dir;
     }
 }
