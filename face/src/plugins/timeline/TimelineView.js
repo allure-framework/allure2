ec99a29d05ba8725d861ff6c8dd9912d6a393495
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2016-10-20 21:39:40 +0300

    upgrade d3js to v4

--- face/src/plugins/timeline/TimelineView.js
+++ face/src/plugins/timeline/TimelineView.js
@@ -3,7 +3,10 @@ import BaseChartView from '../../components/chart/BaseChartView';
 import {once} from 'underscore';
 import {className} from '../../decorators';
 import duration from '../../helpers/duration';
-import d3 from 'd3';
+
+import {scaleLinear, scaleBand} from 'd3-scale';
+import 'd3-selection-multi';
+
 import escape from '../../util/escape';
 import TooltipView from '../../components/tooltip/TooltipView';
 
@@ -17,7 +20,7 @@ const PAD_RIGHT = 10;
 @className('timeline')
 class TimelineView extends BaseChartView {
     initialize() {
-        this.x = d3.scale.linear();
+        this.x = scaleLinear();
         this.tooltip = new TooltipView({position: 'bottom'});
     }
 
@@ -32,7 +35,7 @@ class TimelineView extends BaseChartView {
     }
 
     doShow() {
-        const width = this.$el.width() - PAD_LEFT - PAD_RIGHT;
+        const width = this.$el.width() > PAD_LEFT + PAD_RIGHT ? this.$el.width() - PAD_LEFT - PAD_RIGHT : this.$el.width();
 
         const [from, to] = this.model.getAllTestcases().reduce(
             ([from, to], {time}) => [Math.min(from, time.start), Math.max(to, time.stop)],
@@ -43,7 +46,6 @@ class TimelineView extends BaseChartView {
         this.x.domain([0, to - from]).nice();
         this.x.range([0, width]);
 
-
         this.svg = this.setupViewport();
 
         var currentHeight = 0;
@@ -51,10 +53,9 @@ class TimelineView extends BaseChartView {
             currentHeight += this.drawHost(host, currentHeight);
         });
 
-        this.makeAxis(this.svg.select('.chart__axis_x'), {
+        this.makeBottomAxis(this.svg.select('.chart__axis_x'), {
             tickFormat: d => duration(d, 2),
-            scale: this.x,
-            orient: 'bottom'
+            scale: this.x
         }, {
             top: currentHeight,
             left: PAD_LEFT
@@ -77,20 +78,21 @@ class TimelineView extends BaseChartView {
                 })),
             []
         );
-        const y = d3.scale.ordinal()
+        const y = scaleBand()
             .domain([''].concat(host.children.map(d => d.name)))
-            .rangeRoundBands([0, height]);
-        const group = this.svg.select('.chart__plot').append('g').attr({
+            .range([0, height]);
+
+        const group = this.svg.select('.chart__plot').append('g').attrs({
             'class': 'timeline__group',
             transform: `translate(${PAD_LEFT},${offset})`
         });
 
-        group.append('rect').attr({
+        group.append('rect').attrs({
             'class': 'timeline__host-bg',
             width: Math.max(0, this.x.range()[1] - this.x.range()[0]),
-            height: y.range()[1]
+            height: y.step()
         });
-        group.append('text').text(host.name).attr({
+        group.append('text').text(host.name).attrs({
             'class': 'timeline__host',
             y: BAR_HEIGHT * 0.7,
             x: 10
@@ -99,13 +101,13 @@ class TimelineView extends BaseChartView {
         var bars = group.selectAll('.timeline__item')
             .data(testcases).enter()
             .append('a')
-            .attr({
+            .attrs({
                 'xlink:href': d => '#timeline/' + d.uid
             })
             .append('rect')
-            .attr({
+            .attrs({
                 'class': d => 'timeline__item chart__fill_status_' + d.status,
-                rx: 2, ry: 2,
+                rx: 1, ry: 1,
                 width: 0,
                 height: BAR_HEIGHT - BAR_GAP * 2,
                 x: this.x(0),
@@ -116,15 +118,14 @@ class TimelineView extends BaseChartView {
         if(this.firstRender) {
             bars = bars.transition().duration(500);
         }
-        bars.attr({
+        bars.attrs({
             x: d => this.x(d.start),
             width: d => this.x(Math.max(d.stop - d.start))
         });
-        this.makeAxis(group.append('g').classed('chart__axis', true), {
+        this.makeLeftAxis(group.append('g').classed('chart__axis', true), {
             tickSize: 0,
             tickFormat: '',
-            scale: y,
-            orient: 'left'
+            scale: y
         });
         return height;
     }
