a145f2933bb4c81b5bb0a3f24ae94e4023b71b83
Dmitry Baev <baev.dm@gmail.com>
2016-12-20 18:33:58 +0300

    migrate to gradle (fixes #32)

---
+++ allure-web/webpack.config.js
@@ -0,0 +1,59 @@
+/*eslint-env node*/
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+function makeConfig(development) {
+    return {
+        entry: ['./src/index.js'],
+        output: {
+            path: path.join(__dirname, 'build/resources/main/'),
+            pathinfo: development,
+            filename: 'app.js'
+        },
+        module: {
+            loaders: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }, {
+                test: /\.json$/,
+                loader: 'json-loader'
+            }, {
+                test: /\.(png|svg|woff2?|ttf|eot)(\?.*)?$/,
+                loader: 'url-loader'
+            }, {
+                test: /\.css$/,
+                loader: development ? 'style-loader!css-loader!postcss-loader' : ExtractTextPlugin.extract('style-loader', 'css-loader!postcss-loader')
+            }, {
+                test: /\.hbs$/,
+                loader: 'handlebars-loader',
+                query: {
+                    helperDirs: [
+                        path.join(__dirname, 'src/helpers'),
+                        path.join(__dirname, 'src/blocks')
+                    ]
+                }
+            }]
+        },
+        devtool: development ? 'source-map' : null,
+        plugins: (() => {
+            const plugins = [
+                new ExtractTextPlugin('styles.css')
+            ];
+            if(development) {
+                return [...plugins, new webpack.HotModuleReplacementPlugin()];
+            } else {
+                return [...plugins, new webpack.optimize.UglifyJsPlugin({compress: {unsafe: true}, comments: false})];
+            }
+        })(),
+        postcss: (webpack) => [
+            require('postcss-import')({addDependencyTo: webpack}),
+            require('precss')({'import': {disable: true}}),
+            require('autoprefixer')
+        ]
+    };
+}
+
+module.exports = makeConfig();
+module.exports.factory = makeConfig;
--- commandline/src/it/version/verify.groovy
+++
@@ -1,2 +0,0 @@
-def log = new File(basedir as File, "command.log")
-assert log.text.trim() != 'null'
