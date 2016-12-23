48c4dd4135043e486491a61000bccd6647185070
Dmitry Baev <baev.dm@gmail.com>
2016-12-23 19:38:14 +0300

    fix ie11 issues

--- plugins/packages-plugin/src/dist/static/index.js
+++ plugins/packages-plugin/src/dist/static/index.js
@@ -1,10 +1,14 @@
+'use strict';
 allure.api.addTab('packages', {
     title: 'Packages', icon: 'fa fa-align-left',
     route: 'packages(/:testcaseId)(/:attachmentId)',
-    onEnter: (...routeParams) => new allure.components.TreeLayout({
-        routeParams: routeParams,
-        tabName: 'Packages',
-        baseUrl: 'packages',
-        url: 'data/packages.json'
+    onEnter: (function () {
+        var routeParams = Array.prototype.slice.call(arguments);
+        return new allure.components.TreeLayout({
+            routeParams: routeParams,
+            tabName: 'Packages',
+            baseUrl: 'packages',
+            url: 'data/packages.json'
+        });
     })
 });
