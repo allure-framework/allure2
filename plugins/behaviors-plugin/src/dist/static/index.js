bd77195523924d748fbed9a731e87fe89f919b70
Dmitry Baev <baev.dm@gmail.com>
2016-12-23 19:57:28 +0300

    fix ie11 issues (fixes #46)

--- plugins/behaviors-plugin/src/dist/static/index.js
+++ plugins/behaviors-plugin/src/dist/static/index.js
@@ -1,10 +1,14 @@
+'use strict';
 allure.api.addTab('behaviors', {
     title: 'Behaviors', icon: 'fa fa-list',
     route: 'behaviors(/:testcaseId)(/:attachmentId)',
-    onEnter: (...routeParams) => new allure.components.TreeLayout({
-        routeParams: routeParams,
-        tabName: 'Behaviors',
-        baseUrl: 'behaviors',
-        url: 'data/behaviors.json'
+    onEnter: (function () {
+        var routeParams = Array.prototype.slice.call(arguments);
+        return new allure.components.TreeLayout({
+            routeParams: routeParams,
+            tabName: 'Behaviors',
+            baseUrl: 'behaviors',
+            url: 'data/behaviors.json'
+        });
     })
 });
