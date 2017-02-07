fdc827b68a07f7b75effbdfb2c3224b6dc77755a
Dmitry Baev <baev.dm@gmail.com>
2017-02-07 20:07:36 +0300

    fix comments

--- allure-web/src/helpers/allure-icon.js
+++ allure-web/src/helpers/allure-icon.js
@@ -1,10 +1,13 @@
 import {SafeString} from 'handlebars/runtime';
 
-export default function (value) {
-    switch (value) {
-        case 'flaky':
-            return new SafeString('<span class="fa fa-bomb" data-tooltip="Flaky"></span>');
-        default:
-            return '';
+const icons = {
+    flaky: {
+        className: 'fa fa-bomb',
+        tooltip: 'Test is flaky'
     }
+};
+
+export default function (value) {
+    const icon = icons[value];
+    return icon ? new SafeString(`<span class="${icon.className}" data-tooltip="${icon.tooltip}"></span>`) : '';
 }
