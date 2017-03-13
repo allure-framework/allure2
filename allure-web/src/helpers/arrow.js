282e0dec22949749126f560d8e03409e64e9e7ef
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2017-03-13 15:52:59 +0300

    thanks Boris =) fix some mistakes

--- allure-web/src/helpers/arrow.js
+++ allure-web/src/helpers/arrow.js
@@ -2,7 +2,6 @@ import {SafeString} from 'handlebars/runtime';
 import {values} from '../util/statuses';
 
 
-export default function(item) {
-    var status = (item && item.status) ? item.status : 'unknown';
+export default function(status = 'unknown') {
     return new SafeString(`<span class="fa fa-chevron-right fa-fw text_status_${status}"></span>`);
 }
