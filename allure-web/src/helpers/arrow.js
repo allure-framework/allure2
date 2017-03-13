2e4542231e5f8040a377600dd970f6f7fb10cc4d
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2017-03-13 15:25:58 +0300

    delete unused code

--- allure-web/src/helpers/arrow.js
+++ allure-web/src/helpers/arrow.js
@@ -3,9 +3,6 @@ import {values} from '../util/statuses';
 
 
 export default function(item) {
-    var status = values.slice().reverse().reduce((result, current) => {
-        return item && (item[current] || item.status === current) ? current : result;
-    });
-
+    var status = (item && item.status) ? item.status : 'unknown';
     return new SafeString(`<span class="fa fa-chevron-right fa-fw text_status_${status}"></span>`);
 }
