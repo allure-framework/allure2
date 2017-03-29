7200320ab3727d492623faf74ce48438c7a86a7d
Dmitry Baev <baev.dm@gmail.com>
2017-03-29 18:17:44 +0300

    rft generator test

--- allure-generator/src/test/java/io/qameta/allure/ReportGeneratorTest.java
+++ allure-generator/src/test/java/io/qameta/allure/ReportGeneratorTest.java
@@ -1,59 +1,100 @@
 package io.qameta.allure;
 
-import org.junit.Rule;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static io.qameta.allure.testdata.TestData.unpackDummyResources;
-import static org.junit.Assert.assertThat;
-import static ru.yandex.qatools.matchers.nio.PathMatchers.contains;
-import static ru.yandex.qatools.matchers.nio.PathMatchers.hasFilesCount;
-import static ru.yandex.qatools.matchers.nio.PathMatchers.isDirectory;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author charlie (Dmitry Baev).
  */
 public class ReportGeneratorTest {
 
-    @Rule
-    public TemporaryFolder folder = new TemporaryFolder();
+    @ClassRule
+    public static TemporaryFolder folder = new TemporaryFolder();
 
-    @Test
-    public void shouldGenerateReport() throws Exception {
-        ReportGenerator generator = new ReportGenerator(new DefaultConfiguration());
-        Path output = folder.newFolder().toPath();
-        Path resultsDirectory = folder.newFolder().toPath();
+    private static Path output;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        final ReportGenerator generator = new ReportGenerator(new DefaultConfiguration());
+        output = folder.newFolder().toPath();
+        final Path resultsDirectory = folder.newFolder().toPath();
         unpackDummyResources("allure1data/", resultsDirectory);
         generator.generate(output, resultsDirectory);
-        assertThat(output, contains("index.html"));
-        assertThat(output, contains("data"));
-        Path data = output.resolve("data");
-        assertThat(data, isDirectory());
-
-        assertThat(data, contains("test-cases"));
-        Path testCases = data.resolve("test-cases");
-        assertThat(testCases, isDirectory());
-        assertThat(testCases, hasFilesCount(20, "*.json"));
-
-        assertThat(data, contains("attachments"));
-        Path attachments = data.resolve("attachments");
-        assertThat(attachments, isDirectory());
-        assertThat(attachments, hasFilesCount(13));
-
-        assertThat(data, contains("categories.json"));
-        assertThat(data, contains("graph.json"));
-        assertThat(data, contains("timeline.json"));
-        assertThat(data, contains("widgets.json"));
-        assertThat(data, contains("xunit.json"));
-
-        assertThat(output, contains("history"));
-        Path history = output.resolve("history");
-        assertThat(history, contains("history.json"));
-
-        assertThat(output, contains("export"));
-        Path export = output.resolve("export");
-        assertThat(export, contains("mail.html"));
     }
+
+    @Test
+    public void shouldGenerateIndexHtml() throws Exception {
+        assertThat(output.resolve("index.html"))
+                .isRegularFile();
+    }
+
+    @Test
+    public void shouldGenerateCategoriesJson() throws Exception {
+        assertThat(output.resolve("data/categories.json"))
+                .isRegularFile();
+    }
+
+    @Test
+    public void shouldGenerateGraphJson() throws Exception {
+        assertThat(output.resolve("data/graph.json"))
+                .isRegularFile();
+    }
+
+    @Test
+    public void shouldGenerateXunitJson() throws Exception {
+        assertThat(output.resolve("data/xunit.json"))
+                .isRegularFile();
+    }
+
+    @Test
+    public void shouldGenerateTimelineJson() throws Exception {
+        assertThat(output.resolve("data/timeline.json"))
+                .isRegularFile();
+    }
+
+    @Test
+    public void shouldGenerateWidgetsJson() throws Exception {
+        assertThat(output.resolve("data/widgets.json"))
+                .isRegularFile();
+    }
+
+    @Test
+    public void shouldGenerateAttachments() throws Exception {
+        final Path attachmentsFolder = output.resolve("data/attachments");
+        assertThat(attachmentsFolder)
+                .isDirectory();
+        assertThat(Files.list(attachmentsFolder))
+                .hasSize(13);
+    }
+
+    @Test
+    public void shouldGenerateTestCases() throws Exception {
+        final Path testCasesFolder = output.resolve("data/test-cases");
+        assertThat(testCasesFolder)
+                .isDirectory();
+        assertThat(Files.list(testCasesFolder))
+                .hasSize(20);
+    }
+
+    @Test
+    public void shouldGenerateHistory() throws Exception {
+        assertThat(output.resolve("history/history.json"))
+                .isRegularFile();
+    }
+
+    @Test
+    public void shouldGenerateMail() throws Exception {
+        assertThat(output.resolve("export/mail.html"))
+                .isRegularFile();
+    }
+
+
 }
