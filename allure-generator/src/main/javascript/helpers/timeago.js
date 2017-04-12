12259ba732ea08f3b884bea0fb659f788d1cee38
Dmitry Baev <baev.dm@gmail.com>
2017-04-12 16:23:24 +0300

    fix comments

--- allure-generator/src/main/javascript/helpers/timeago.js
+++ allure-generator/src/main/javascript/helpers/timeago.js
@@ -1,8 +1,10 @@
-import moment from 'moment';
-import runtime from 'injectify/runtime';
+import {SafeString} from 'handlebars/runtime';
+import distanceInWordsToNow from 'date-fns/distance_in_words_to_now';
+import format from 'date-fns/format';
+
+const threshold = 24 * 3600 * 1000;
 
 export default function (date) {
-    const threshold = 24 * 3600 * 1000;
-    const dateString = Date.now() - date < threshold ? moment(date).fromNow() : moment(date).format('DD MMMM YYYY');
-    return new runtime.SafeString(`<span title="${moment(date).format('DD MMMM YYYY, H:mm:ss')}">${dateString}</span>`);
+    const dateString = Date.now() - date < threshold ? distanceInWordsToNow(date) : format(date, 'DD MMMM YYYY');
+    return new SafeString(`<span title="${format('DD MMMM YYYY, H:mm:ss')}">${dateString}</span>`);
 }
