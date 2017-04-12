154ae5ce1ae7f03a1b1eb07dc8cf2d496c8eec30
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2017-04-12 22:33:33 +0300

    fix trend chart

--- allure-generator/src/main/javascript/plugins/graph/charts/TestTrendChart.js
+++ allure-generator/src/main/javascript/plugins/graph/charts/TestTrendChart.js
@@ -26,80 +26,39 @@ class TestTrendGraphView extends BaseChartView {
         } else {
             this.$el.html('<div class="widget__noitems">There are nothing to show</div>');
         }
-
-        // const s = stack()
-        //     .keys(values)
-        //     .value((d, key) => d[key] || 0);
-        //
-        // const a = area()
-        //     .x(d => this.x(d.data.id))
-        //     .y0(d => this.y(d[0]))
-        //     .y1(d => this.y(d[1]));
-        //
-        // this.y.domain([0, max(
-        //     data,
-        //     d => sum(entries(d), l => set(values).has(l.key) ? l.value : 0))
-        // ]);
-        // this.x.domain(extent(data, d => d.id));
-        //
-        // this.makeBottomAxis(this.svg.select('.chart__axis_x'), {
-        //         scale: this.x,
-        //         tickFormat: d => '#' + d
-        //     }, {
-        //         top: 400,
-        //         left: 15
-        //     }
-        // );
-        // this.makeLeftAxis(this.svg.select('.chart__axis_y'), {
-        //     scale: this.y,
-        //     ticks: Y_AMOUNT
-        // }, {
-        //     left: 400,
-        //     top: 15
-        // });
-
-        // this.svg.select('.chart__plot').attrs({transform: `translate(${PAD_LEFT},${PAD_TOP})`});
-        //
-        // const layer = this.svg
-        //     .selectAll('.layer')
-        //     .data(s(data))
-        //     .enter()
-        //     .append('g')
-        //     .attr('class', 'layer');
-        //
-        // layer.append('path')
-        //     .attr('class', 'area')
-        //     .attr('d', a)
-        //     .attr('class', d => 'trend_status_' + d.key)
-        //     .style('opacity', .8);
-
         super.onAttach();
     }
 
     doShow(data) {
-        const width = 600;
-        const height = 300;
+        this.svg = this.setupViewport();
+        const height = this.$el.outerHeight() - PAD_BOTTOM - PAD_TOP;
+        const width = this.$el.outerWidth() - PAD_LEFT - PAD_RIGHT;
 
-        console.log(width);
-        console.log(height);
+        const s = stack()
+            .keys(values)
+            .value((d, key) => d[key] || 0);
+
+        const a = area()
+            .x(d =>  this.x(d.data.id))
+            .y0(d => this.y(d[0]))
+            .y1(d => this.y(d[1]));
 
         this.x.range([0, width]);
         this.y.range([height, 0], 1);
 
-        this.svg = this.setupViewport();
+        const maxY = max(data, d => d.total);
 
-        this.x.domain([0, data.length]).nice();
+        this.x.domain([0, data.length-1]).nice();
+        this.y.domain([0, maxY]);
 
         this.makeBottomAxis(this.svg.select('.chart__axis_x'), {
             scale: this.x,
-            tickFormat: d => '#' + d
+            tickFormat: d => '#' + d,
+            ticks: data.length
         }, {
             top: PAD_TOP + height,
             left: PAD_LEFT
         });
-
-        const maxY = max(data, d => d.total);
-        this.y.domain([0, maxY]);
 
         this.makeLeftAxis(this.svg.select('.chart__axis_y'), {
             scale: this.y,
@@ -109,18 +68,9 @@ class TestTrendGraphView extends BaseChartView {
             top: PAD_TOP
         });
 
-        this.svg.select('.chart__plot').attrs({transform: `translate(${PAD_LEFT},${PAD_TOP})`});
+       var plot =  this.svg.select('.chart__plot').attrs({transform: `translate(${PAD_LEFT},${PAD_TOP})`});
 
-        const s = stack()
-            .keys(values)
-            .value((d, key) => d[key] || 0);
-
-        const a = area()
-            .x(d => this.x(d.data.id))
-            .y0(d => this.y(d[0]))
-            .y1(d => this.y(d[1]));
-
-        const layer = this.svg
+        const layer = plot
             .selectAll('.layer')
             .data(s(data))
             .enter()
@@ -135,6 +85,8 @@ class TestTrendGraphView extends BaseChartView {
     }
 
     getChartData() {
+        var length = this.options.items.length - 1;
+        this.options.items.forEach((d,i)=> d['id'] = length - i)
         return this.options.items;
     }
 }
