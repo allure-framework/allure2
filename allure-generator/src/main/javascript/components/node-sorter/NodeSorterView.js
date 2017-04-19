53ea94786fdcc56ea121c47b817349b42cb98627
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2017-04-20 00:20:26 +0300

    fix after review

--- allure-generator/src/main/javascript/components/node-sorter/NodeSorterView.js
+++ allure-generator/src/main/javascript/components/node-sorter/NodeSorterView.js
@@ -13,17 +13,17 @@ class NodeSorterView extends View {
     sorters = [
         {
             key: 'name',
-            sorter: (a, b, c) => {return a.name.toLowerCase() < b.name.toLowerCase() ? -c : c;}
+            sorter: (a, b) => {return a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1;}
         },
         {
             key: 'duration',
-            sorter: (a, b, c) => {return a.time.duration < b.time.duration ? -c : c;}
+            sorter: (a, b) => {return a.time.duration < b.time.duration ? -1 : 1;}
         },
         {
             key: 'status',
-            sorter: (a, b, c) => {
+            sorter: (a, b) => {
                 if ('status' in a && 'status' in b){
-                    return values.indexOf(a.status) > values.indexOf(b.status) ? -c : c;
+                    return values.indexOf(a.status) > values.indexOf(b.status) ? -1 : 1;
                 } else if ('statistic' in a && 'statistic' in b){
                     return values.reduce((all, current) => {
                         if ((a.statistic[current] !== b.statistic[current]) && all === 0) {
@@ -31,7 +31,7 @@ class NodeSorterView extends View {
                         } else {
                             return all;
                         }
-                    }, 0) ? -c: c;
+                    }, 0) ? -1: 1;
                 } else {
                     return 1;
                 }
@@ -47,21 +47,23 @@ class NodeSorterView extends View {
         const sortSettings = settings.getTreeSorting(this.sorterSettingsKey);
         const sorter = this.sorters[sortSettings.sorter].sorter;
         const direction =  sortSettings.ascending ? 1 : -1;
-        return (a, b) => sorter(a, b, direction);
+        return (a, b) => sorter(a, b) * direction;
     }
 
     @on('click .sorter__item')
     onChangeSorting(e){
         const el = this.$(e.currentTarget);
+
         settings.save(this.sorterSettingsKey, {
             sorter: el.data('index'),
             ascending: !el.data('asc')
         });
 
+        const ascending = el.data('asc');
         this.$('.sorter_enabled').toggleClass('sorter_enabled');
-        el.data('asc', !el.data('asc'));
+        el.data('asc', !ascending);
         el.find('.sorter__name').toggleClass('sorter_enabled');
-        el.find(el.data('asc')? '.fa-sort-asc' : '.fa-sort-desc').toggleClass('sorter_enabled');
+        el.find(ascending? '.fa-sort-asc' : '.fa-sort-desc').toggleClass('sorter_enabled');
     }
 
     serializeData() {
