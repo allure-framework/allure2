1a32244954bc56692810b33280a41ab907b93dd2
Stanislav Seliverstov <s.seliverstov@gmail.com>
2017-04-20 15:42:07 +0300

    sort nodes in tree (fixes #79, via #282)

--- allure-generator/src/main/javascript/components/tree/TreeView.js
+++ allure-generator/src/main/javascript/components/tree/TreeView.js
@@ -3,21 +3,27 @@ import {View} from 'backbone.marionette';
 import settings from '../../util/settings';
 import template from './TreeView.hbs';
 import StatusToggleView from '../status-toggle/StatusToggleView';
-import {on} from '../../decorators';
+import NodeSorterView from '../node-sorter/NodeSorterView';
+import {on, regions} from '../../decorators';
 import {behavior} from '../../decorators/index';
 
 @behavior('TooltipBehavior', {position: 'bottom'})
+@regions({sorter: '.tree__sorter'})
 class TreeView extends View {
     template = template;
 
-    initialize({state, tabName, baseUrl}) {
+    initialize({state, treeState, tabName, baseUrl}) {
+        this.treeState = treeState;
         this.state = state;
         this.baseUrl = baseUrl;
         this.tabName = tabName;
         this.statusesKey = tabName + '.visibleStatuses';
+        this.sorterSettingsKey = tabName + '.treeSorting';
         this.statusesSelect = new StatusToggleView({statusesKey: this.statusesKey});
-        this.listenTo(this.state, 'change:testcase', (m, testcase) => this.highlightItem(testcase));
+        this.nodeSorter = new NodeSorterView({sorterSettingsKey: this.sorterSettingsKey});
+        this.listenTo(this.state, 'change:testcase', (m, testcase) => this.restoreState(testcase));
         this.listenTo(settings, 'change:' + this.statusesKey, this.render);
+        this.listenTo(settings, 'change:' + this.sorterSettingsKey, this.render);
         this.listenTo(settings, 'change:showGroupInfo', this.render);
     }
 
@@ -26,7 +32,8 @@ class TreeView extends View {
     }
 
     onRender() {
-        this.highlightItem(this.state.get('testcase'));
+        this.restoreState(this.state.get('testcase'));
+        this.showChildView('sorter', new NodeSorterView({sorterSettingsKey: this.sorterSettingsKey}));
     }
 
     onDestroy() {
@@ -36,6 +43,12 @@ class TreeView extends View {
     @on('click .node__title')
     onNodeClick(e) {
         this.$(e.currentTarget).parent().toggleClass('node__expanded');
+        const uid = this.$(e.currentTarget).data('uid');
+        if (uid in  this.treeState) {
+            delete this.treeState[uid];
+        } else {
+            this.treeState[uid] = true;
+        }
     }
 
     @on('click .tree__statuses')
@@ -54,39 +67,42 @@ class TreeView extends View {
         settings.save('showGroupInfo', !show);
     }
 
-    highlightItem(uid) {
+    restoreState(uid) {
         this.$('[data-uid]').each((i, node) => {
             const el = this.$(node);
             el.toggleClass('node__title_active', el.data('uid') === uid);
+            el.toggleClass('node__expanded', (el.data('uid') in this.treeState));
         });
     }
 
     serializeData() {
         const statuses = settings.getVisibleStatuses(this.statusesKey);
+        const sorter = this.nodeSorter.getSorter();
         const showGroupInfo = settings.get('showGroupInfo');
+
         return {
             baseUrl: this.baseUrl,
             showGroupInfo: showGroupInfo,
             time: this.collection.time,
             statistic: this.collection.statistic,
             tabName: this.tabName,
-            items: this.filterNodes(statuses, this.collection.toJSON())
+            items: this.filterNodes(statuses, sorter, this.collection.toJSON()),
         };
     }
 
-    filterNodes(statuses, nodes) {
+    filterNodes(statuses, sorter, nodes) {
         return nodes
-            .map(item => this.mapNode(statuses, item))
+            .map(item => this.mapNode(statuses, sorter, item))
             .filter(item => item.type === 'TestCaseNode' ? statuses[item.status] : item.children.length > 0)
-            .sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1);
+            .sort(sorter);
     }
 
-    mapNode(statuses, node) {
+    mapNode(statuses, sorter, node) {
         if (node.type === 'TestCaseNode') {
             return node;
         }
         return Object.assign({}, node, {
-            children: this.filterNodes(statuses, node.children)
+            children: this.filterNodes(statuses, sorter, node.children)
         });
     }
 }
