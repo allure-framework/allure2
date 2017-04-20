43fc2c5d792de02f745e530265503a9d8b8b9be4
Artem Eroshenko <eroshenkoam@yandex-team.ru>
2017-04-20 17:40:54 +0300

    remove wildcard imports

--- allure-generator/src/test/java/io/qameta/allure/testdata/TestData.java
+++ allure-generator/src/test/java/io/qameta/allure/testdata/TestData.java
@@ -10,7 +10,11 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
