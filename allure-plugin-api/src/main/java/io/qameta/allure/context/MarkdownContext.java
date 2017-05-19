0f0d140b7639c9c4faf9b44ca611927c30a513aa
Dmitry Baev <baev.dm@gmail.com>
2017-05-19 17:42:20 +0300

    use flexmark instead of pegdown (via #340)

--- allure-plugin-api/src/main/java/io/qameta/allure/context/MarkdownContext.java
+++ allure-plugin-api/src/main/java/io/qameta/allure/context/MarkdownContext.java
@@ -1,20 +1,22 @@
 package io.qameta.allure.context;
 
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
 import io.qameta.allure.Context;
-import org.pegdown.Extensions;
-import org.pegdown.PegDownProcessor;
+
+import java.util.function.Function;
 
 /**
  * Markdown context. Can be used to process markdown files to html.
  *
  * @since 2.0
  */
-public class MarkdownContext implements Context<PegDownProcessor> {
-
-    private final PegDownProcessor processor = new PegDownProcessor(Extensions.ALL);
+public class MarkdownContext implements Context<Function<String, String>> {
 
     @Override
-    public PegDownProcessor getValue() {
-        return processor;
+    public Function<String, String> getValue() {
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return s -> renderer.render(parser.parse(s));
     }
 }
