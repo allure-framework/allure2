77c9959873b0548a5a94ddfda0d176d1b6b5f4ff
Egor Borisov <ehborisov@gmail.com>
2017-05-19 19:25:13 +0400

    add more components to plugin api (via #349)

--- allure-generator/src/main/javascript/pluginApi.js
+++ allure-generator/src/main/javascript/pluginApi.js
@@ -1,10 +1,12 @@
 import pluginsRegistry from './util/pluginsRegistry';
 import TreeLayout from './layouts/tree/TreeLayout';
+import AppLayout from './layouts/application/AppLayout';
 import WidgetStatusView from './components/widget-status/WidgetStatusView';
 
 window.allure = {
     api: pluginsRegistry,
     components: {
+        AppLayout: AppLayout,
         TreeLayout: TreeLayout,
         WidgetStatusView: WidgetStatusView
     }
