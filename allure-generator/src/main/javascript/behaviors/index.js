3aca089b7a1b6508783c44f84c609523e9dba03c
Dmitry Baev <baev.dm@gmail.com>
2017-07-11 20:33:56 +0300

    add copy to clipboard behavior

--- allure-generator/src/main/javascript/behaviors/index.js
+++ allure-generator/src/main/javascript/behaviors/index.js
@@ -1,4 +1,5 @@
 import LoadBehavior from './LoadBehavior';
 import TooltipBehavior from './TooltipBehavior';
+import ClipboardBehavior from './ClipboardBehavior';
 
-export {LoadBehavior, TooltipBehavior};
+export {LoadBehavior, TooltipBehavior, ClipboardBehavior};
