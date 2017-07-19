23772f1603de4000bf440866f0592365095cc133
Dmitry Baev <baev.dm@gmail.com>
2017-07-19 16:51:24 +0300

    add more methods to steps

--- allure-plugin-api/src/main/java/io/qameta/allure/entity/WithSummary.java
+++ allure-plugin-api/src/main/java/io/qameta/allure/entity/WithSummary.java
@@ -2,6 +2,8 @@ package io.qameta.allure.entity;
 
 import javax.xml.bind.annotation.XmlElement;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 import static java.util.Collections.emptyList;
 import static java.util.Objects.isNull;
@@ -10,6 +12,8 @@ import static java.util.Objects.isNull;
  * @author charlie (Dmitry Baev).
  */
 public interface WithSummary {
+
+    StatusDetails getStatusDetails();
 
     List<Step> getSteps();
 
@@ -37,10 +41,26 @@ public interface WithSummary {
     }
 
     @XmlElement
+    default boolean shouldDisplayMessage() {
+        final Optional<String> message = Optional.ofNullable(getStatusDetails())
+                .map(StatusDetails::getMessage);
+        return message.isPresent() && getSteps().stream()
+                .noneMatch(step -> step.hasMessage(message.get()));
+    }
+
+    default boolean hasMessage(String message) {
+        final Optional<String> current = Optional.ofNullable(getStatusDetails())
+                .map(StatusDetails::getMessage)
+                .filter(s -> Objects.equals(s, message));
+        return current.isPresent() || getSteps().stream()
+                .anyMatch(step -> step.hasMessage(message));
+    }
+
+    @XmlElement
     default boolean hasContent() {
         final List<Attachment> attachments = isNull(getAttachments()) ? emptyList() : getAttachments();
         final List<Step> steps = isNull(getSteps()) ? emptyList() : getSteps();
         final List<Parameter> parameters = isNull(getParameters()) ? emptyList() : getParameters();
-        return steps.size() + attachments.size() + parameters.size() > 0;
+        return steps.size() + attachments.size() + parameters.size() > 0 || shouldDisplayMessage();
     }
 }
