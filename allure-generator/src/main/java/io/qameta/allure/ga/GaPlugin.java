e1258ff7e3d3dfa871217079df015aafea0ba4c8
Artem Eroshenko <eroshenkoam@qameta.io>
2017-08-06 16:10:32 +0300

    calculate client id

--- allure-generator/src/main/java/io/qameta/allure/ga/GaPlugin.java
+++ allure-generator/src/main/java/io/qameta/allure/ga/GaPlugin.java
@@ -4,6 +4,7 @@ import io.qameta.allure.Aggregator;
 import io.qameta.allure.core.Configuration;
 import io.qameta.allure.core.LaunchResults;
 import io.qameta.allure.entity.ExecutorInfo;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
@@ -14,8 +15,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.URI;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -35,6 +43,10 @@ import static io.qameta.allure.executor.ExecutorPlugin.EXECUTORS_BLOCK_NAME;
 public class GaPlugin implements Aggregator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GaPlugin.class);
+
+    private static final String EXECUTOR_TYPE_LOCAL = "Local";
+
+    private static final String VERSION_UNDEFINED = "Undefined";
 
     private static final String GA_DISABLE = "ALLURE_NO_ANALYTICS";
 
@@ -56,22 +68,34 @@ public class GaPlugin implements Aggregator {
                 .mapToLong(Collection::size)
                 .sum();
 
-        final String executor = launchesResults.stream()
+        final String executorType = launchesResults.stream()
                 .map(results -> results.<ExecutorInfo>getExtra(EXECUTORS_BLOCK_NAME))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .findFirst()
                 .map(ExecutorInfo::getType)
-                .orElse("Local");
+                .orElse(EXECUTOR_TYPE_LOCAL);
+
+        final String hostname = launchesResults.stream()
+                .map(results -> results.<ExecutorInfo>getExtra(EXECUTORS_BLOCK_NAME))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .map(ExecutorInfo::getBuildUrl)
+                .map(URI::create)
+                .map(URI::getHost)
+                .orElse(getLocalHost());
+
+        final String clientId = DigestUtils.sha256Hex(hostname);
 
         final String allureVersion = Optional.of(getClass())
                 .map(Class::getPackage)
                 .map(Package::getImplementationVersion)
-                .orElse("Undefined");
+                .orElse(VERSION_UNDEFINED);
 
         try {
             CompletableFuture
-                    .runAsync(() -> sendStats(allureVersion, testResultsCount, executor))
+                    .runAsync(() -> sendStats(clientId, allureVersion, testResultsCount, executorType))
                     .get(10, TimeUnit.SECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             LOGGER.debug("Could not send analytics within 10 seconds", e);
@@ -79,7 +103,8 @@ public class GaPlugin implements Aggregator {
     }
 
     @SuppressWarnings("EmptyTryBlock")
-    protected void sendStats(final String allureVersion, final long testResultsCount, final String executor) {
+    protected void sendStats(final String clientId, final String allureVersion,
+                             final long testResultsCount, final String executor) {
         final HttpClientBuilder builder = HttpClientBuilder.create();
         try (CloseableHttpClient client = builder.build()) {
             List<NameValuePair> pairs = Arrays.asList(
@@ -89,7 +114,7 @@ public class GaPlugin implements Aggregator {
                     pair("z", UUID.randomUUID().toString()),
                     pair("t", "event"),
                     pair("ds", "allure cli"),
-                    pair("cid", UUID.randomUUID().toString()),
+                    pair("cid", clientId),
                     pair("an", "Allure Report"),
                     pair("ec", "Allure CLI events"),
                     pair("ea", "Report generate"),
@@ -105,6 +130,23 @@ public class GaPlugin implements Aggregator {
             LOGGER.debug("GA done");
         } catch (IOException e) {
             LOGGER.debug("Could not send analytics", e);
+        }
+    }
+
+    private static String getLocalHost() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            LOGGER.debug("Could not get host name {}", e);
+            return "default";
+        }
+    }
+
+    public static MessageDigest getMd5Digest() {
+        try {
+            return MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Can not find hashing algorithm", e);
         }
     }
 
