f9ad6f722d9878c26f2b31220045c5beb8d732e6
Artem Eroshenko <eroshenkoam@qameta.io>
2017-08-06 20:11:33 +0300

    add results format to analytics

--- allure-generator/src/main/java/io/qameta/allure/ga/GaPlugin.java
+++ allure-generator/src/main/java/io/qameta/allure/ga/GaPlugin.java
@@ -4,6 +4,8 @@ import io.qameta.allure.Aggregator;
 import io.qameta.allure.core.Configuration;
 import io.qameta.allure.core.LaunchResults;
 import io.qameta.allure.entity.ExecutorInfo;
+import io.qameta.allure.entity.Label;
+import io.qameta.allure.entity.LabelName;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -30,6 +32,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 import static io.qameta.allure.executor.ExecutorPlugin.EXECUTORS_BLOCK_NAME;
 
@@ -60,48 +63,28 @@ public class GaPlugin implements Aggregator {
             return;
         }
         LOGGER.debug("send analytics");
-        final long testResultsCount = launchesResults.stream()
-                .map(LaunchResults::getResults)
-                .mapToLong(Collection::size)
-                .sum();
-
-        final String executorType = launchesResults.stream()
-                .map(results -> results.<ExecutorInfo>getExtra(EXECUTORS_BLOCK_NAME))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .findFirst()
-                .map(ExecutorInfo::getType)
-                .orElse(EXECUTOR_TYPE_LOCAL);
-
-        final Optional<String> executorHostName = launchesResults.stream()
-                .map(results -> results.<ExecutorInfo>getExtra(EXECUTORS_BLOCK_NAME))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .findFirst()
-                .map(ExecutorInfo::getBuildUrl)
-                .map(URI::create)
-                .map(URI::getHost);
-
-        final String clientId = DigestUtils.sha256Hex(executorHostName
-                .orElse(getLocalHostName().orElse(UUID.randomUUID().toString())));
-
         final String allureVersion = Optional.of(getClass())
                 .map(Class::getPackage)
                 .map(Package::getImplementationVersion)
                 .orElse(VERSION_UNDEFINED);
 
+        final String cid = getClientId(launchesResults);
+        final String executorType = getExecutorType(launchesResults);
+        final long testResultsCount = getTestResultsCount(launchesResults);
+        final String testResultFormats = getResultsFormats(launchesResults);
+
         try {
             CompletableFuture
-                    .runAsync(() -> sendStats(clientId, allureVersion, testResultsCount, executorType))
+                    .runAsync(() -> sendStats(cid, allureVersion, executorType, testResultFormats, testResultsCount))
                     .get(10, TimeUnit.SECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
             LOGGER.debug("Could not send analytics within 10 seconds", e);
         }
     }
 
-    @SuppressWarnings("EmptyTryBlock")
-    protected void sendStats(final String clientId, final String allureVersion,
-                             final long testResultsCount, final String executor) {
+    @SuppressWarnings("PMD.UseObjectForClearerAPI")
+    protected void sendStats(final String clientId, final String allureVersion, final String executorType,
+                             final String resultsFormats, final long testResultsCount) {
         final HttpClientBuilder builder = HttpClientBuilder.create();
         try (CloseableHttpClient client = builder.build()) {
             List<NameValuePair> pairs = Arrays.asList(
@@ -117,7 +100,8 @@ public class GaPlugin implements Aggregator {
                     pair("ea", "Report generate"),
                     pair("av", allureVersion),
                     pair("ds", "Report generator"),
-                    pair("cd2", executor),
+                    pair("cd2", executorType),
+                    pair("cd4", resultsFormats),
                     pair("cm1", String.valueOf(testResultsCount))
             );
             final HttpPost post = new HttpPost(GA_ENDPOINT);
@@ -128,6 +112,47 @@ public class GaPlugin implements Aggregator {
         } catch (IOException e) {
             LOGGER.debug("Could not send analytics", e);
         }
+    }
+
+    private static String getClientId(final List<LaunchResults> launchesResults) {
+        final Optional<String> executorHostName = launchesResults.stream()
+                .map(results -> results.<ExecutorInfo>getExtra(EXECUTORS_BLOCK_NAME))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .map(ExecutorInfo::getBuildUrl)
+                .map(URI::create)
+                .map(URI::getHost);
+        return DigestUtils.sha256Hex(executorHostName
+                .orElse(getLocalHostName().orElse(UUID.randomUUID().toString())));
+    }
+
+    private static String getExecutorType(final List<LaunchResults> launchesResults) {
+        return launchesResults.stream()
+                .map(results -> results.<ExecutorInfo>getExtra(EXECUTORS_BLOCK_NAME))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .findFirst()
+                .map(ExecutorInfo::getType)
+                .orElse(EXECUTOR_TYPE_LOCAL);
+    }
+
+    private static long getTestResultsCount(final List<LaunchResults> launchesResults) {
+        return launchesResults.stream()
+                .map(LaunchResults::getResults)
+                .mapToLong(Collection::size)
+                .sum();
+    }
+
+    private static String getResultsFormats(final List<LaunchResults> launchesResults) {
+        return launchesResults.stream()
+                .flatMap(results -> results.getResults().stream())
+                .flatMap(result -> result.getLabels().stream())
+                .filter(label -> LabelName.RESULT_FORMAT.value().equals(label.getName()))
+                .map(Label::getValue)
+                .distinct()
+                .sorted()
+                .collect(Collectors.joining(" "));
     }
 
     private static Optional<String> getLocalHostName() {
