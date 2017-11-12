0e5579b1b3baf2f742f89f98b326a7a7e4c16a6a
Stas Seliverstov <s.seliverstov@gmail.com>
2017-11-12 16:57:15 +0300

    graphs

---
+++ allure-generator/src/main/javascript/data/trend/TrendCollection.js
@@ -0,0 +1,38 @@
+import {Collection} from 'backbone';
+import {uniq, flatten, keys, values, omit} from 'underscore';
+
+
+export default class TrendCollection extends Collection {
+
+    initialize(models, options) {
+        this.options = options;
+    }
+
+    url() {
+        return `widgets/${this.options.name}.json`;
+    }
+
+    parse(response) {
+        return response
+            .reverse()
+            .map((item, id) => {
+                return {
+                    ...item,
+                    id,
+                    name: item.buildOrder ? `#${item.buildOrder}` : '',
+                    total: values(omit(item.data, 'total')).reduce((prev, curr) => prev + curr, 0),
+                    data: omit(item.data, 'total')
+                };
+            });
+    }
+
+    keys() {
+        return uniq(flatten(this.map(model => keys(model.get('data')))));
+    }
+
+    sortedKeysByLastValue() {
+        const allKeys = this.keys();
+        const lastData = this.last().get('data');
+        return allKeys.sort((a, b) => (lastData[b] || 0) - (lastData[a] || 0));
+    }
+}
