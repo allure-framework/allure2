6988c3d50562c6e9f606b81f7652ece258ccf49d
jenkl <jenkl@yandex-team.ru>
2018-08-09 12:42:05 +0300

    add labels to the prometheus metrics

--- allure-generator/src/main/java/io/qameta/allure/prometheus/PrometheusMetricLine.java
+++ allure-generator/src/main/java/io/qameta/allure/prometheus/PrometheusMetricLine.java
@@ -16,17 +16,23 @@ public class PrometheusMetricLine implements MetricLine {
     private final String name;
     private final String key;
     private final String value;
+    private final String labels;
 
     @Override
     public String asString() {
-        return String.format("%s_%s %s",
+        return String.format("%s_%s%s %s",
                 getName(),
                 normalize(getKey()),
+                normalizeLabels(getLabels()),
                 getValue()
         );
     }
 
     public static String normalize(final String string) {
         return string.toLowerCase().replaceAll("\\s+", "_");
+    }
+
+    private String normalizeLabels(final String labels) {
+        return labels == null ? "" : "{" + labels + "}";
     }
 }
