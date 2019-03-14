c7f65eb292fcc54a58b01e1fddad3e9c643353ed
Valery Yatsynovich <valfirst@yandex.ru>
2019-03-14 13:52:50 +0300

    extract common for all trend plugins logic to a new superclass (via #913)

--- allure-generator/src/main/java/io/qameta/allure/category/CategoriesTrendPlugin.java
+++ allure-generator/src/main/java/io/qameta/allure/category/CategoriesTrendPlugin.java
@@ -19,93 +19,34 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.qameta.allure.CommonJsonAggregator;
-import io.qameta.allure.CompositeAggregator;
 import io.qameta.allure.Constants;
-import io.qameta.allure.Reader;
-import io.qameta.allure.context.JacksonContext;
-import io.qameta.allure.core.Configuration;
 import io.qameta.allure.core.LaunchResults;
-import io.qameta.allure.core.ResultsVisitor;
-import io.qameta.allure.entity.ExecutorInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.qameta.allure.trend.AbstractTrendPlugin;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.Spliterator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static io.qameta.allure.executor.ExecutorPlugin.EXECUTORS_BLOCK_NAME;
-import static java.util.Comparator.comparing;
-import static java.util.Comparator.naturalOrder;
-import static java.util.Comparator.nullsFirst;
-import static java.util.Spliterators.spliteratorUnknownSize;
-import static java.util.stream.StreamSupport.stream;
 
 /**
  * Plugin that generates data for Categories-trend graph.
  */
-@SuppressWarnings("PMD.ExcessiveImports")
-public class CategoriesTrendPlugin extends CompositeAggregator implements Reader {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(CategoriesTrendPlugin.class);
+public class CategoriesTrendPlugin extends AbstractTrendPlugin<CategoriesTrendItem> {
 
     private static final String JSON_FILE_NAME = "categories-trend.json";
 
     public static final String CATEGORIES_TREND_BLOCK_NAME = "categories-trend";
 
     public CategoriesTrendPlugin() {
-        super(Arrays.asList(
-                new JsonAggregator(), new WidgetAggregator()
-        ));
+        super(Arrays.asList(new JsonAggregator(), new WidgetAggregator()), JSON_FILE_NAME, CATEGORIES_TREND_BLOCK_NAME);
     }
 
     @Override
-    public void readResults(final Configuration configuration,
-                            final ResultsVisitor visitor,
-                            final Path directory) {
-        final JacksonContext context = configuration.requireContext(JacksonContext.class);
-        final Path historyFile = directory.resolve(Constants.HISTORY_DIR).resolve(JSON_FILE_NAME);
-        if (Files.exists(historyFile)) {
-            try (InputStream is = Files.newInputStream(historyFile)) {
-                final ObjectMapper mapper = context.getValue();
-                final JsonNode jsonNode = mapper.readTree(is);
-                final List<CategoriesTrendItem> history = getStream(jsonNode)
-                        .map(child -> parseItem(historyFile, mapper, child))
-                        .filter(Optional::isPresent)
-                        .map(Optional::get)
-                        .collect(Collectors.toList());
-
-                visitor.visitExtra(CATEGORIES_TREND_BLOCK_NAME, history);
-            } catch (IOException e) {
-                visitor.error("Could not read categories-trend file " + historyFile, e);
-            }
-        }
-    }
-
-    private Stream<JsonNode> getStream(final JsonNode jsonNode) {
-        return stream(
-                spliteratorUnknownSize(jsonNode.elements(), Spliterator.ORDERED),
-                false);
-    }
-
-    private Optional<CategoriesTrendItem> parseItem(final Path historyFile,
-                                                    final ObjectMapper mapper,
-                                                    final JsonNode child) {
-        try {
-            return Optional.ofNullable(mapper.treeToValue(child, CategoriesTrendItem.class));
-        } catch (JsonProcessingException e) {
-            LOGGER.warn("Could not read {}", historyFile, e);
-            return Optional.empty();
-        }
+    protected Optional<CategoriesTrendItem> parseItem(final ObjectMapper mapper, final JsonNode child)
+            throws JsonProcessingException {
+        return Optional.ofNullable(mapper.treeToValue(child, CategoriesTrendItem.class));
     }
 
     protected static List<CategoriesTrendItem> getData(final List<LaunchResults> launchesResults) {
@@ -143,17 +84,6 @@ public class CategoriesTrendPlugin extends CompositeAggregator implements Reader
         return item;
     }
 
-    private static Optional<ExecutorInfo> extractLatestExecutor(final List<LaunchResults> launches) {
-        final Comparator<ExecutorInfo> comparator = comparing(ExecutorInfo::getBuildOrder, nullsFirst(naturalOrder()));
-        return launches.stream()
-                .map(launch -> launch.getExtra(EXECUTORS_BLOCK_NAME))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .filter(ExecutorInfo.class::isInstance)
-                .map(ExecutorInfo.class::cast)
-                .max(comparator);
-    }
-
     /**
      * Generates history trend data.
      */
@@ -175,7 +105,7 @@ public class CategoriesTrendPlugin extends CompositeAggregator implements Reader
     private static class WidgetAggregator extends CommonJsonAggregator {
 
         WidgetAggregator() {
-            super("widgets", JSON_FILE_NAME);
+            super(Constants.WIDGETS_DIR, JSON_FILE_NAME);
         }
 
         @Override
--- allure-generator/src/main/java/io/qameta/allure/retry/RetryTrendPlugin.java
+++ allure-generator/src/main/java/io/qameta/allure/retry/RetryTrendPlugin.java
@@ -15,97 +15,39 @@
  */
 package io.qameta.allure.retry;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.qameta.allure.CommonJsonAggregator;
-import io.qameta.allure.CompositeAggregator;
-import io.qameta.allure.Constants;
-import io.qameta.allure.Reader;
-import io.qameta.allure.context.JacksonContext;
-import io.qameta.allure.core.Configuration;
-import io.qameta.allure.core.LaunchResults;
-import io.qameta.allure.core.ResultsVisitor;
-import io.qameta.allure.entity.ExecutorInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.Spliterator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static io.qameta.allure.executor.ExecutorPlugin.EXECUTORS_BLOCK_NAME;
-import static java.util.Comparator.comparing;
-import static java.util.Comparator.naturalOrder;
-import static java.util.Comparator.nullsFirst;
-import static java.util.Spliterators.spliteratorUnknownSize;
-import static java.util.stream.StreamSupport.stream;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.qameta.allure.CommonJsonAggregator;
+import io.qameta.allure.Constants;
+import io.qameta.allure.core.LaunchResults;
+import io.qameta.allure.trend.AbstractTrendPlugin;
 
 /**
  * Plugin that generates data for Retry-Trend graph.
  */
-@SuppressWarnings("PMD.ExcessiveImports")
-public class RetryTrendPlugin extends CompositeAggregator implements Reader {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(RetryTrendPlugin.class);
+public class RetryTrendPlugin extends AbstractTrendPlugin<RetryTrendItem> {
 
     private static final String JSON_FILE_NAME = "retry-trend.json";
 
     public static final String RETRY_TREND_BLOCK_NAME = "retry-trend";
 
     public RetryTrendPlugin() {
-        super(Arrays.asList(
-                new JsonAggregator(), new WidgetAggregator()
-        ));
+        super(Arrays.asList(new JsonAggregator(), new WidgetAggregator()), JSON_FILE_NAME, RETRY_TREND_BLOCK_NAME);
     }
 
     @Override
-    public void readResults(final Configuration configuration,
-                            final ResultsVisitor visitor,
-                            final Path directory) {
-        final JacksonContext context = configuration.requireContext(JacksonContext.class);
-        final Path historyFile = directory.resolve(Constants.HISTORY_DIR).resolve(JSON_FILE_NAME);
-        if (Files.exists(historyFile)) {
-            try (InputStream is = Files.newInputStream(historyFile)) {
-                final ObjectMapper mapper = context.getValue();
-                final JsonNode jsonNode = mapper.readTree(is);
-                final List<RetryTrendItem> history = getStream(jsonNode)
-                        .map(child -> parseItem(historyFile, mapper, child))
-                        .filter(Optional::isPresent)
-                        .map(Optional::get)
-                        .collect(Collectors.toList());
-
-                visitor.visitExtra(RETRY_TREND_BLOCK_NAME, history);
-            } catch (IOException e) {
-                visitor.error("Could not read retry-trend file " + historyFile, e);
-            }
-        }
-    }
-
-    private Stream<JsonNode> getStream(final JsonNode jsonNode) {
-        return stream(
-                spliteratorUnknownSize(jsonNode.elements(), Spliterator.ORDERED),
-                false);
-    }
-
-    private Optional<RetryTrendItem> parseItem(final Path historyFile,
-                                               final ObjectMapper mapper,
-                                               final JsonNode child) {
-        try {
-            return Optional.ofNullable(mapper.treeToValue(child, RetryTrendItem.class));
-        } catch (JsonProcessingException e) {
-            LOGGER.warn("Could not read {}", historyFile, e);
-            return Optional.empty();
-        }
+    protected Optional<RetryTrendItem> parseItem(final ObjectMapper mapper, final JsonNode child)
+            throws JsonProcessingException {
+        return Optional.ofNullable(mapper.treeToValue(child, RetryTrendItem.class));
     }
 
     @SuppressWarnings("PMD.DefaultPackage")
@@ -142,17 +84,6 @@ public class RetryTrendPlugin extends CompositeAggregator implements Reader {
                 .flatMap(launch -> launch.getAllResults().stream())
                 .forEach(item::update);
         return item;
-    }
-
-    private static Optional<ExecutorInfo> extractLatestExecutor(final List<LaunchResults> launches) {
-        final Comparator<ExecutorInfo> comparator = comparing(ExecutorInfo::getBuildOrder, nullsFirst(naturalOrder()));
-        return launches.stream()
-                .map(launch -> launch.getExtra(EXECUTORS_BLOCK_NAME))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .filter(ExecutorInfo.class::isInstance)
-                .map(ExecutorInfo.class::cast)
-                .max(comparator);
     }
 
     /**
