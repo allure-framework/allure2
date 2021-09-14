47beb925f7578d2f897c2987a5a9390259c3a603
PNe-ins <58724452+PNe-ins@users.noreply.github.com>
2021-09-14 13:44:03 +0200

    add support for inner test results for trx (via #1082)

--- plugins/trx-plugin/src/test/java/io/qameta/allure/trx/TrxPluginTest.java
+++ plugins/trx-plugin/src/test/java/io/qameta/allure/trx/TrxPluginTest.java
@@ -19,9 +19,16 @@ import io.qameta.allure.Issue;
 import io.qameta.allure.context.RandomUidContext;
 import io.qameta.allure.core.Configuration;
 import io.qameta.allure.core.ResultsVisitor;
+import io.qameta.allure.entity.Label;
 import io.qameta.allure.entity.LabelName;
 import io.qameta.allure.entity.Status;
 import io.qameta.allure.entity.TestResult;
+
+import static io.qameta.allure.entity.LabelName.RESULT_FORMAT;
+import static io.qameta.allure.entity.LabelName.PACKAGE;
+import static io.qameta.allure.entity.LabelName.SUITE;
+import static io.qameta.allure.entity.LabelName.TEST_CLASS;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -31,8 +38,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -136,6 +145,44 @@ class TrxPluginTest {
                 .filteredOn(result -> result.getTestStage().getSteps().get(1).getName().contains("Given I have entered 50 into the calculator"))
                 .filteredOn(result -> result.getTestStage().getSteps().get(3).getName().contains("And I have entered -1 into the calculator"))
                 .hasSize(1);
+    }
+
+    @Test
+    void shouldParseTestResultChildren() throws Exception {
+        process(
+                "trxdata/testresultsWithChildren.trx",
+                "sample.trx"
+        );
+
+        final ArgumentCaptor<TestResult> captor = ArgumentCaptor.forClass(TestResult.class);
+        verify(visitor, times(7)).visitTestResult(captor.capture());
+
+        assertThat(captor.getAllValues())
+                .hasSize(7)
+                .extracting(TestResult::getName, TestResult::getStatus)
+                .containsExactlyInAnyOrder(
+                        tuple("UnitTest_One", Status.PASSED),
+                        tuple("UnitTest_Two", Status.PASSED),
+                        tuple("UnitTest_Three", Status.FAILED),
+                        tuple("UnitTest_Three (Child One)", Status.FAILED),
+                        tuple("UnitTest_Three (Child Two)", Status.FAILED),
+                        tuple("UnitTest_Three (Child Two) (GrandChild One)", Status.PASSED),
+                        tuple("UnitTest_Three (Child Two) (GrandChild Two)", Status.FAILED)
+                );
+
+        List<Label> labels = new ArrayList<>();
+        labels.add(new Label().setName(SUITE.value()).setValue("Test"));
+        labels.add(new Label().setName(TEST_CLASS.value()).setValue("Test"));
+        labels.add(new Label().setName(PACKAGE.value()).setValue("Test"));
+        labels.add(new Label().setName(RESULT_FORMAT.value()).setValue("trx"));
+
+        assertThat(captor.getAllValues())
+                .filteredOn(result -> result.getName().contains("UnitTest_Three"))
+                .extracting(TestResult::getLabels)
+                .containsOnly(
+                        labels
+                );
+
     }
 
     private void process(String... strings) throws IOException {
