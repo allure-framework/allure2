5384b735d41b58b15b998263e4e55c9d501100d6
Dmitry Baev <baev@users.noreply.github.com>
2021-10-12 13:25:27 +0300

    add support for tables in descriptions

--- allure-plugin-api/src/main/java/io/qameta/allure/context/MarkdownContext.java
+++ allure-plugin-api/src/main/java/io/qameta/allure/context/MarkdownContext.java
@@ -15,10 +15,13 @@
  */
 package io.qameta.allure.context;
 
+import com.vladsch.flexmark.ext.tables.TablesExtension;
 import com.vladsch.flexmark.html.HtmlRenderer;
 import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.data.MutableDataSet;
 import io.qameta.allure.Context;
 
+import java.util.Arrays;
 import java.util.function.Function;
 
 /**
@@ -28,10 +31,15 @@ import java.util.function.Function;
  */
 public class MarkdownContext implements Context<Function<String, String>> {
 
+    private static final MutableDataSet OPTIONS = new MutableDataSet()
+            .set(HtmlRenderer.SOFT_BREAK, "<br />\n")
+            .set(HtmlRenderer.SUPPRESS_HTML, true)
+            .set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create()));
+
     @Override
     public Function<String, String> getValue() {
-        final Parser parser = Parser.builder().build();
-        final HtmlRenderer renderer = HtmlRenderer.builder().build();
+        final Parser parser = Parser.builder(OPTIONS).build();
+        final HtmlRenderer renderer = HtmlRenderer.builder(OPTIONS).build();
         return s -> renderer.render(parser.parse(s));
     }
 }
