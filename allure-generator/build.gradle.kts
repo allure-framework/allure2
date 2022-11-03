aed3bb94003d9a9b644e1440d641bd423ccc6a3e
Dmitry Baev <baev.dm@gmail.com>
2022-11-03 14:09:07 +0700

    bump node from 14 to 16 (via #1800)

--- allure-generator/build.gradle.kts
+++ allure-generator/build.gradle.kts
@@ -10,8 +10,7 @@ description = "Allure Report Generator"
 node {
     // enforce https
     distBaseUrl.set("https://nodejs.org/dist")
-    version.set("14.16.1")
-    npmVersion.set("6.14.12")
+    version.set("16.18.0")
     download.set(true)
 }
 
