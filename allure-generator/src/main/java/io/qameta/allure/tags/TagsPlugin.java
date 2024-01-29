8be75a7488fd439e7363b4e28ebc26bdd10f0e1c
Dmitry Baev <baev@users.noreply.github.com>
2024-01-29 14:45:59 +0000

    parse labels from tags using naming convention

--- allure-generator/src/main/java/io/qameta/allure/tags/TagsPlugin.java
+++ allure-generator/src/main/java/io/qameta/allure/tags/TagsPlugin.java
@@ -19,17 +19,26 @@ import io.qameta.allure.Aggregator2;
 import io.qameta.allure.ReportStorage;
 import io.qameta.allure.core.Configuration;
 import io.qameta.allure.core.LaunchResults;
+import io.qameta.allure.entity.Label;
 import io.qameta.allure.entity.LabelName;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * @author charlie (Dmitry Baev).
  */
 public class TagsPlugin implements Aggregator2 {
+
+    private static final Pattern LABEL_TAG = Pattern.compile("^@?allure\\.label\\.(?<name>.+)[:=](?<value>.+)$");
 
     public static final String TAGS_BLOCK_NAME = "tags";
 
@@ -41,8 +50,42 @@ public class TagsPlugin implements Aggregator2 {
                 .map(LaunchResults::getAllResults)
                 .flatMap(Collection::stream)
                 .forEach(result -> {
-                    final Set<String> tags = new HashSet<>(result.findAllLabels(LabelName.TAG));
-                    result.addExtraBlock(TAGS_BLOCK_NAME, tags);
+                    final Set<String> tags = result.findAllLabels(LabelName.TAG, Collectors.toSet());
+
+                    final List<Label> extraLabels = tags.stream()
+                            .filter(Objects::nonNull)
+                            .filter(tag -> LABEL_TAG.matcher(tag).matches())
+                            .map(TagsPlugin::createFromTag)
+                            .filter(Optional::isPresent)
+                            .map(Optional::get)
+                            .collect(Collectors.toList());
+
+                    if (!extraLabels.isEmpty()) {
+                        final List<Label> labels = new ArrayList<>(result.getLabels());
+                        labels.addAll(extraLabels);
+                        result.setLabels(extraLabels);
+                    }
+
+                    final Set<String> filteredTags = tags.stream()
+                            .filter(Objects::nonNull)
+                            .filter(tag -> !LABEL_TAG.matcher(tag).matches())
+                            .map(String::trim)
+                            .filter(s -> !"".equals(s))
+                            .collect(Collectors.toSet());
+
+                    result.addExtraBlock(TAGS_BLOCK_NAME, new HashSet<>(filteredTags));
                 });
+    }
+
+    public static Optional<Label> createFromTag(final String tag) {
+        final Matcher label = LABEL_TAG.matcher(tag);
+        if (label.matches()) {
+            final String name = label.group("name");
+            final String value = label.group("value");
+            return Optional.of(new Label()
+                    .setName(name)
+                    .setValue(value.replace("_", " ")));
+        }
+        return Optional.empty();
     }
 }
---
+++ allure-generator/src/test/java/io/qameta/allure/tags/TagsPluginTest.java
@@ -0,0 +1,314 @@
+/*
+ *  Copyright 2016-2024 Qameta Software Inc
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.qameta.allure.tags;
+
+import io.qameta.allure.DefaultConfiguration;
+import io.qameta.allure.DefaultLaunchResults;
+import io.qameta.allure.ReportStorage;
+import io.qameta.allure.core.Configuration;
+import io.qameta.allure.core.LaunchResults;
+import io.qameta.allure.entity.Label;
+import io.qameta.allure.entity.TestResult;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+class TagsPluginTest {
+
+    @Test
+    void shouldAddTagsFromLabels() {
+        final TestResult testResult = new TestResult()
+                .setLabels(Arrays.asList(
+                        new Label()
+                                .setName("not a tag")
+                                .setValue("some value"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("first"),
+                        new Label()
+                                .setName("feature")
+                                .setValue("Auth"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("second"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("third")
+                ));
+
+        final List<LaunchResults> launchResults = List.of(
+                new DefaultLaunchResults(Set.of(testResult), Map.of(), Map.of())
+        );
+
+        final Configuration configuration
+                = new DefaultConfiguration(List.of(), List.of());
+
+        final ReportStorage storage = mock();
+
+        new TagsPlugin().aggregate(
+                configuration,
+                launchResults,
+                storage
+        );
+
+        assertThat(testResult.<Set<String>>getExtraBlock(TagsPlugin.TAGS_BLOCK_NAME))
+                .containsExactlyInAnyOrder(
+                        "first",
+                        "second",
+                        "third"
+                );
+    }
+
+    @Test
+    void shouldRemoveDuplicateTags() {
+        final TestResult testResult = new TestResult()
+                .setLabels(Arrays.asList(
+                        new Label()
+                                .setName("not a tag")
+                                .setValue("some value"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("first"),
+                        new Label()
+                                .setName("feature")
+                                .setValue("Auth"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("first"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("second"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("    first    "),
+                        new Label()
+                                .setName("tag")
+                                .setValue("third")
+                ));
+
+        final List<LaunchResults> launchResults = List.of(
+                new DefaultLaunchResults(Set.of(testResult), Map.of(), Map.of())
+        );
+
+        final Configuration configuration
+                = new DefaultConfiguration(List.of(), List.of());
+
+        final ReportStorage storage = mock();
+
+        new TagsPlugin().aggregate(
+                configuration,
+                launchResults,
+                storage
+        );
+
+        assertThat(testResult.<Set<String>>getExtraBlock(TagsPlugin.TAGS_BLOCK_NAME))
+                .containsExactlyInAnyOrder(
+                        "first",
+                        "second",
+                        "third"
+                );
+    }
+
+    @Test
+    void shouldTrimTagNames() {
+        final TestResult testResult = new TestResult()
+                .setLabels(Arrays.asList(
+                        new Label()
+                                .setName("not a tag")
+                                .setValue("some value"),
+                        new Label()
+                                .setName("tag")
+                                .setValue(" first  \n "),
+                        new Label()
+                                .setName("feature")
+                                .setValue("Auth"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("  second"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("third")
+                ));
+
+        final List<LaunchResults> launchResults = List.of(
+                new DefaultLaunchResults(Set.of(testResult), Map.of(), Map.of())
+        );
+
+        final Configuration configuration
+                = new DefaultConfiguration(List.of(), List.of());
+
+        final ReportStorage storage = mock();
+
+        new TagsPlugin().aggregate(
+                configuration,
+                launchResults,
+                storage
+        );
+
+        assertThat(testResult.<Set<String>>getExtraBlock(TagsPlugin.TAGS_BLOCK_NAME))
+                .containsExactlyInAnyOrder(
+                        "first",
+                        "second",
+                        "third"
+                );
+    }
+
+    @Test
+    void shouldParseLabelsWithoutName() {
+        final TestResult testResult = new TestResult()
+                .setLabels(Arrays.asList(
+                        new Label()
+                                .setValue("some value"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("first"),
+                        new Label()
+                                .setName("feature")
+                                .setValue("Auth"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("second"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("third")
+                ));
+
+        final List<LaunchResults> launchResults = List.of(
+                new DefaultLaunchResults(Set.of(testResult), Map.of(), Map.of())
+        );
+
+        final Configuration configuration
+                = new DefaultConfiguration(List.of(), List.of());
+
+        final ReportStorage storage = mock();
+
+        new TagsPlugin().aggregate(
+                configuration,
+                launchResults,
+                storage
+        );
+
+        assertThat(testResult.<Set<String>>getExtraBlock(TagsPlugin.TAGS_BLOCK_NAME))
+                .containsExactlyInAnyOrder(
+                        "first",
+                        "second",
+                        "third"
+                );
+    }
+
+    @Test
+    void shouldParseLabelsWithoutValue() {
+        final TestResult testResult = new TestResult()
+                .setLabels(Arrays.asList(
+                        new Label()
+                                .setName("tag"),
+                        new Label()
+                                .setName("feature"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("second"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("third")
+                ));
+
+        final List<LaunchResults> launchResults = List.of(
+                new DefaultLaunchResults(Set.of(testResult), Map.of(), Map.of())
+        );
+
+        final Configuration configuration
+                = new DefaultConfiguration(List.of(), List.of());
+
+        final ReportStorage storage = mock();
+
+        new TagsPlugin().aggregate(
+                configuration,
+                launchResults,
+                storage
+        );
+
+        assertThat(testResult.<Set<String>>getExtraBlock(TagsPlugin.TAGS_BLOCK_NAME))
+                .containsExactlyInAnyOrder(
+                        "second",
+                        "third"
+                );
+    }
+
+    @Test
+    void shouldAddMetaTags() {
+        final TestResult testResult = new TestResult()
+                .setLabels(Arrays.asList(
+                        new Label()
+                                .setName("not a tag")
+                                .setValue("some value"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("first"),
+                        new Label()
+                                .setName("feature")
+                                .setValue("Auth"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("second"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("@allure.label.story=Some_story"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("@allure.label.parentSuite:Regression"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("@allure.label.suite:Search_Articles"),
+                        new Label()
+                                .setName("tag")
+                                .setValue("@allure.label.subSuite:Mobile")
+                ));
+
+        final List<LaunchResults> launchResults = List.of(
+                new DefaultLaunchResults(Set.of(testResult), Map.of(), Map.of())
+        );
+
+        final Configuration configuration
+                = new DefaultConfiguration(List.of(), List.of());
+
+        final ReportStorage storage = mock();
+
+        new TagsPlugin().aggregate(
+                configuration,
+                launchResults,
+                storage
+        );
+
+        assertThat(testResult.getLabels())
+                .extracting(Label::getName, Label::getValue)
+                .contains(
+                        tuple("story", "Some story"),
+                        tuple("parentSuite", "Regression"),
+                        tuple("suite", "Search Articles"),
+                        tuple("subSuite", "Mobile")
+                );
+    }
+}
