ec31eca8a5160106a4e06e991ef75361495cbc1e
Yaroslav Serhieiev <noomorph@gmail.com>
2024-03-13 17:54:50 +0200

    Simplify 'Severity' template

--- allure-generator/src/main/javascript/plugins/testresult-severity/SeverityView.js
+++ allure-generator/src/main/javascript/plugins/testresult-severity/SeverityView.js
@@ -1,22 +1,13 @@
 import { View } from "backbone.marionette";
-import { escapeExpression } from "handlebars/runtime";
 import { className } from "../../decorators/index";
 import translate from "../../helpers/t";
-
-const severities = ["blocker", "critical", "normal", "minor", "trivial"];
 
 @className("pane__section")
 class SeverityView extends View {
   template({ severity }) {
-    if (!severity) {
-      return "";
-    }
-
-    if (severities.indexOf(severity) >= 0) {
-      return `${translate("testResult.severity.name")}: ${translate(`testResult.severity.${severity}`)}`;
-    }
-
-    return `${translate("testResult.severity.name")}: ${escapeExpression(severity)}`;
+    return severity
+      ? `${translate("testResult.severity.name")}: ${translate(`testResult.severity.${severity}`)}`
+      : "";
   }
 
   serializeData() {
