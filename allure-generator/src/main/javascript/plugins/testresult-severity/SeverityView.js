a59f8f1e13db3dfcc126036e723d424de6403641
Yaroslav Serhieiev <noomorph@gmail.com>
2024-03-14 16:26:17 +0200

    make 'Severity' translatable (via #2396)

--- allure-generator/src/main/javascript/plugins/testresult-severity/SeverityView.js
+++ allure-generator/src/main/javascript/plugins/testresult-severity/SeverityView.js
@@ -1,13 +1,12 @@
 import { View } from "backbone.marionette";
-import { escapeExpression } from "handlebars/runtime";
 import { className } from "../../decorators/index";
 import translate from "../../helpers/t";
 
 @className("pane__section")
 class SeverityView extends View {
-  template(data) {
-    return data.severity
-      ? `${translate("testResult.severity.name")}: ${escapeExpression(data.severity)}`
+  template({ severity }) {
+    return severity
+      ? `${translate("testResult.severity.name")}: ${translate(`testResult.severity.${severity}`)}`
       : "";
   }
 
