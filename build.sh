6433161904554c42121b69e3d18bfbcb52ce0f07
sdd <sdd@lab.local>
2024-08-07 22:33:30 +0700

    🚧 build project

--- build.sh
+++ build.sh
@@ -2,16 +2,21 @@
 rm -rf build
 rm -rf ./allure-commandline/build
 rm -rf ./allure-generator/build
+rm -rf ./allure-generator/node_modules
+# rm -rf ./allure-generator/.gradle
 rm -rf ./allure-jira-commons/build
 rm -rf ./allure-plugin-api/build
 rm -rf allure-report
 
-./gradlew build buildRpm buildDeb -Pversion=2.30.0-il
+# build deb
+./gradlew build buildRpm buildDeb -Pversion=2.30.0-1-il
 
+# install allure (LINUX)
 cd ./allure-commandline/build/distributions
-sudo dpkg -i allure_2.30.0~il-1_all.deb
+sudo dpkg -i allure_2.30.0~1~il-1_all.deb
 allure --version
 
+# Server Allure
 cd ../../../
 allure generate logs -c - report
-allure open -p 9000 allure-report
+allure open -p 9001 allure-report
--- del_buld.sh
+++ del_buld.sh
@@ -2,6 +2,8 @@
 rm -rf build
 rm -rf ./allure-commandline/build
 rm -rf ./allure-generator/build
+rm -rf ./allure-generator/node_modules
+# rm -rf ./allure-generator/.gradle
 rm -rf ./allure-jira-commons/build
 rm -rf ./allure-plugin-api/build
 rm -rf allure-report
