aa7a122741c5516ae6b1138b36c67e90d85df840
Nathan Buache <44255541+nthnbch@users.noreply.github.com>
2024-10-22 10:31:06 +0200

    Update index.js

--- plugins/behaviors-plugin/src/dist/static/index.js
+++ plugins/behaviors-plugin/src/dist/static/index.js
@@ -210,7 +210,6 @@ allure.api.addTranslation('isv', {
     }
 });
 
-
 allure.api.addTranslation('ka', {
     tab: {
         behaviors: {
@@ -225,6 +224,19 @@ allure.api.addTranslation('ka', {
     }
 });
 
+allure.api.addTranslation('it', {
+    tab: {
+        behaviors: {
+            name: 'Comportamenti'
+        }
+    },
+    widget: {
+        behaviors: {
+            name: 'Funzionalità per storie',
+            showAll: 'Mostra tutto'
+        }
+    }
+});
 
 allure.api.addTab('behaviors', {
     title: 'tab.behaviors.name', icon: 'fa fa-list',
