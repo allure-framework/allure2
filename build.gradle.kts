bc2faf9374fd45bb6d94af3a32503a28e0c6ad42
Valery Yatsynovich <valery_yatsynovich@epam.com>
2025-12-10 19:22:32 +0300

    bump com.opencsv:opencsv from 5.9 to 5.12.0 (via #3111)

--- build.gradle.kts
+++ build.gradle.kts
@@ -90,7 +90,7 @@ subprojects {
             dependency("ch.qos.logback:logback-classic:1.3.16")
             dependency("com.beust:jcommander:1.82")
             dependency("com.github.spotbugs:spotbugs:4.9.8")
-            dependency("com.opencsv:opencsv:5.9")
+            dependency("com.opencsv:opencsv:5.12.0")
             dependency("com.puppycrawl.tools:checkstyle:12.2.0")
             dependency("com.squareup.retrofit2:converter-jackson:${comSquareupRetrofit2Version}")
             dependency("com.squareup.retrofit2:retrofit:${comSquareupRetrofit2Version}")
