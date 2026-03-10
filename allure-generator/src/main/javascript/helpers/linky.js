491d9801f0e12725ba1cb65118d98c1d15637291
vdvukhzhilov <vdvukhzhilov@gmail.com>
2026-03-10 15:43:06 +0100

    fix: prevent xss in parameters and steps

--- allure-generator/src/main/javascript/helpers/linky.js
+++ allure-generator/src/main/javascript/helpers/linky.js
@@ -1,9 +1,15 @@
-import { SafeString } from "handlebars/runtime";
+import { escapeExpression, SafeString } from "handlebars/runtime";
 
 const URL_REGEXP = /^(\w)+:\/\/.*/;
 
 export default function (text) {
-  return URL_REGEXP.test(text)
-    ? new SafeString(`<a href="${text}"  class="link" target="_blank">${text}</a>`)
-    : text;
+  if (!URL_REGEXP.test(text)) {
+    return text;
+  }
+
+  const safeText = escapeExpression(text);
+
+  return new SafeString(
+    `<a href="${safeText}" class="link" target="_blank" rel="noopener noreferrer">${safeText}</a>`
+  );
 }
