edbfcd5b38f5075e5c7ee482e9a2b7dd5bb42bb9
epszaw <konstantin@epishev.me>
2026-05-11 17:33:56 +0200

    remove legacy flag from the service config

--- allurerc.mjs
+++ allurerc.mjs
@@ -3,7 +3,6 @@ const { ALLURE_SERVICE_TOKEN } = process.env;
 const allureService = ALLURE_SERVICE_TOKEN
     ? {
         accessToken: ALLURE_SERVICE_TOKEN,
-        legacy: true,
     }
     : undefined;
 
